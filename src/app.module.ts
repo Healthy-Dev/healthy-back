08161b597bc855c75e20d3c3bbd37a3420783e0b
franmper <38388588+franmper@users.noreply.github.com>
2020-01-20 21:31:10 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule, ConfigModule.forRoot({envFilePath: 'key.env'})],
   controllers: [AppController],
   providers: [AppService],
 })
