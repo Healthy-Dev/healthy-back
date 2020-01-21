c1487fb187ae15047ca023872363fd877f649867
franmper <38388588+franmper@users.noreply.github.com>
2020-01-20 21:40:02 -0300

    create auth controller, and add route /profile & /auth/login

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Request, Post, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth/auth.service';
+
+@Controller('auth')
+export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+
+    @UseGuards(AuthGuard('local'))
+    @Post('auth/login')
+    async login(@Request() req) {
+      return this.authService.login(req.user);
+    }
+  
+    @UseGuards(AuthGuard('jwt'))
+    @Get('profile')
+    getProfile(@Request() req) {
+      return req.user;
+    }
+}
