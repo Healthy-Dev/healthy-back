fec879519eaeb912a25cec662ab71b0e037b772d
franmper <38388588+franmper@users.noreply.github.com>
2020-01-20 21:39:03 -0300

    delete all routes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,20 +1,6 @@
-import { Controller, Request, Post, Get, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import { AuthService } from './auth/auth.service';
+import { Controller } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly authService: AuthService) {}
 
-  @UseGuards(AuthGuard('local'))
-  @Post('auth/login')
-  async login(@Request() req) {
-    return this.authService.login(req.user);
-  }
-
-  @UseGuards(AuthGuard('jwt'))
-  @Get('profile')
-  getProfile(@Request() req) {
-    return req.user;
-  }
 }
