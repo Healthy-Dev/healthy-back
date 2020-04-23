745d55083c12fbb1e832277947de0d3443a15052
Andres Di Croche <dicrocheandres@gmail.com>
2020-04-23 14:22:24 -0300

    remove unnecessary files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UsersModule, ConfigModule.forRoot({envFilePath: 'key.env'})],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    AuthModule,
+    UsersModule,
+    ConfigModule.forRoot({ envFilePath: 'key.env' }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
