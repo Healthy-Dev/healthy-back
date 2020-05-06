290e97d8cf0207afcd2b9a31ed61d223b6bbea6e
Andres Di Croche <dicrocheandres@gmail.com>
2020-05-06 19:38:47 -0300

    add module controller and service cards

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
+import { CardsModule } from './cards/cards.module';
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig)],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), CardsModule],
   controllers: [AppController],
   providers: [AppService],
 })
