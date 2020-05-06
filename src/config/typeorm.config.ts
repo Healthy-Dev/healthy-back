73b289a3788510c0fd5e7c84d28a979c33f512f4
Andres Di Croche <dicrocheandres@gmail.com>
2020-05-06 19:31:30 -0300

    add typeorm config

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+    type: 'postgres',
+    host: process.env.POSTGRES_HOSTNAME,
+    port: parseInt(process.env.POSTGRES_PORT, 10),
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    entities: [__dirname + '/../**/*.entity.{js,ts}'],
+    synchronize: Boolean(process.env.TYPEORM_SYNCHRONIZE),
+};
