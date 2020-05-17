9e458f5e933f0c0aa987faba6a24fb6e80091d3d
Francisco Peralta <franp@mantik.tech>
2020-05-17 13:14:06 -0300

    add cloudinary miss implementation yet

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CardsModule } from './cards/cards.module';
+
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+  cloud_name: 'du7xgj6ms',
+  api_key: '188197447859445',
+  api_secret: 'kUvtxcl4tkozexP_upTu-hXb7sE',
+});
+
 @Module({
   imports: [TypeOrmModule.forRoot(), CardsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
--- src/cards/cards.service.ts
+++ src/cards/cards.service.ts
@@ -4,12 +4,20 @@ import { Card } from './card.entity';
 import { CardRepository } from './card.repository';
 import { CreateCardDto } from './dto/create-card.dto';
 
+const cloudinary = require('cloudinary').v2;
+
 @Injectable()
 export class CardsService {
     constructor(
         @InjectRepository(Card) private cardRepository: CardRepository,
     ) {}
-    async createCards(createCardsDto: CreateCardDto, file): Promise<Card> {
+    async createCards(createCardsDto: CreateCardDto, file: any): Promise<Card> {
+        console.log(file);
+        await cloudinary.uploader.upload(file.originalname, (error: any, response: any) => {
+            console.log(error);
+            console.log(response)
+        })
+
         return this.cardRepository.createCards(createCardsDto, file);
     }
 }
