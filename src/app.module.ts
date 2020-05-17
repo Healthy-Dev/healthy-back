605f420482a50d21a643e973c252793b86891cd8
Francisco Peralta <franp@mantik.tech>
2020-05-17 14:26:39 -0300

    enviroment variables for cloudinary

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,9 @@ import { CardsModule } from './cards/cards.module';
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-  cloud_name: 'du7xgj6ms',
-  api_key: '188197447859445',
-  api_secret: 'kUvtxcl4tkozexP_upTu-hXb7sE',
+  cloud_name: process.env.CLOUDINARY_USER,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 @Module({
--- src/cards/cards.module.ts
+++ src/cards/cards.module.ts
@@ -3,6 +3,7 @@ import { CardsController } from './cards.controller';
 import { CardsService } from './cards.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CardRepository } from './card.repository';
+
 @Module({
   imports: [TypeOrmModule.forFeature([CardRepository])],
   controllers: [CardsController],
