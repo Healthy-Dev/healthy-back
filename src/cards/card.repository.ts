f7b527dd8d4ab30a58160062c4c89f25d4f3614f
Francisco Peralta <franp@mantik.tech>
2020-05-18 21:16:03 -0300

    change the response

--- src/cards/card.repository.ts
+++ src/cards/card.repository.ts
@@ -4,7 +4,7 @@ import { CreateCardDto } from './dto/create-card.dto';
 
 @EntityRepository(Card)
 export class CardRepository extends Repository<Card> {
-  async createCards(createCardDto: CreateCardDto, file: string): Promise<Card> {
+  async createCards(createCardDto: CreateCardDto, file: string): Promise<{}> {
     const { title, description, externalUrl } = createCardDto;
 
     const card = new Card();
@@ -15,6 +15,6 @@ export class CardRepository extends Repository<Card> {
 
     await card.save();
 
-    return card;
+    return { idOfTheNewCard: card.id };
   }
 }
