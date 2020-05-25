1da14da55944c281be78c62ff98edf3bcad10392
Francisco Peralta <franp@mantik.tech>
2020-05-25 18:23:02 -0300

    fix indentation and variable return name

--- src/cards/cards.service.ts
+++ src/cards/cards.service.ts
@@ -10,42 +10,40 @@ const cloudinary = require('cloudinary').v2;
 
 @Injectable()
 export class CardsService {
-   constructor(
-      @InjectRepository(Card) private cardRepository: CardRepository,
-   ) {}
-   async getCards(filterDto: GetCardsFilterDto): Promise<CardPreviewDto[]> {
-      return this.cardRepository.getCards(filterDto);
-   }
-   async createCards(
-      createCardsDto: CreateCardDto,
-      file: any = 'http://res.cloudinary.com/du7xgj6ms/image/upload/v1589734759/tcu6xvx0hh62iyys05fs.jpg',
-   ): Promise<{}> {
-      let photoUrl: string = '';
-      if (file.buffer) {
-         const photoInBase64 = file.buffer.toString('base64');
-         const imageType = file.mimetype;
-         await cloudinary.uploader.upload(
-            `data:${imageType};base64,${photoInBase64}`,
-            {
-               format: 'jpg',
-               resource_type: 'image',
-               width: 500,
-               height: 500,
-               crop: 'limit',
-               background: '#03111F',
-            },
-            (error: any, response: any) => {
-               if (error) {
-                  throw error;
-               } else {
-                  photoUrl = response.url;
-               }
-            },
-         );
-      }
-      return this.cardRepository.createCards(
-         createCardsDto,
-         photoUrl ? photoUrl : file,
+  constructor(@InjectRepository(Card) private cardRepository: CardRepository) {}
+  async getCards(filterDto: GetCardsFilterDto): Promise<CardPreviewDto[]> {
+    return this.cardRepository.getCards(filterDto);
+  }
+  async createCards(
+    createCardsDto: CreateCardDto,
+    file: any = 'http://res.cloudinary.com/du7xgj6ms/image/upload/v1589734759/tcu6xvx0hh62iyys05fs.jpg',
+  ): Promise<{}> {
+    let photoUrl: string = '';
+    if (file.buffer) {
+      const photoInBase64 = file.buffer.toString('base64');
+      const imageType = file.mimetype;
+      await cloudinary.uploader.upload(
+        `data:${imageType};base64,${photoInBase64}`,
+        {
+          format: 'jpg',
+          resource_type: 'image',
+          width: 500,
+          height: 500,
+          crop: 'limit',
+          background: '#03111F',
+        },
+        (error: any, response: any) => {
+          if (error) {
+            throw error;
+          } else {
+            photoUrl = response.url;
+          }
+        },
       );
-   }
+    }
+    return this.cardRepository.createCards(
+      createCardsDto,
+      photoUrl ? photoUrl : file,
+    );
+  }
 }
