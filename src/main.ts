5a5682a6afdf877ca8fd65bf041bc5e12c6ae75e
Andres Di Croche <dicrocheandres@gmail.com>
2020-05-25 23:25:00 -0300

    add config deploy heroku

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  await app.listen(port);
 }
 bootstrap();
