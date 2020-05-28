2e7501458acd50a513e841c70b0d20adfe8c01c2
franmper <franp@mantik.tech>
2020-05-28 19:39:14 -0300

    add endpoint get by id

--- src/cards/cards.controller.ts
+++ src/cards/cards.controller.ts
@@ -1,13 +1,27 @@
-import { Controller, Get, UsePipes, ValidationPipe, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  UsePipes,
+  ValidationPipe,
+  Query,
+  ParseIntPipe,
+  Param,
+} from '@nestjs/common';
 import { CardsService } from './cards.service';
 import { GetCardsFilterDto } from './dto/get-cards.dto';
 import { CardPreviewDto } from './dto/card-preview.dto';
+import { Card } from './card.entity';
 @Controller()
 export class CardsController {
-    constructor(private cardsService: CardsService) { }
-    @Get('v1/cards')
-    @UsePipes(new ValidationPipe({ transform: true }))
-    getCards(@Query() filterDto: GetCardsFilterDto): Promise<CardPreviewDto[]> {
-        return this.cardsService.getCards(filterDto);
-    }
+  constructor(private cardsService: CardsService) {}
+  @Get('v1/cards')
+  @UsePipes(new ValidationPipe({ transform: true }))
+  getCards(@Query() filterDto: GetCardsFilterDto): Promise<CardPreviewDto[]> {
+    return this.cardsService.getCards(filterDto);
+  }
+
+  @Get('v1/cards/:id')
+  getCardsById(@Param('id', ParseIntPipe) id: number): Promise<Card> {
+    return this.cardsService.getCardsById(id);
+  }
 }
--- src/cards/cards.service.ts
+++ src/cards/cards.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Card } from './card.entity';
 import { CardRepository } from './card.repository';
@@ -6,10 +6,20 @@ import { GetCardsFilterDto } from './dto/get-cards.dto';
 import { CardPreviewDto } from './dto/card-preview.dto';
 @Injectable()
 export class CardsService {
-    constructor(
-        @InjectRepository(Card) private cardRepository: CardRepository,
-    ) { }
-    async getCards(filterDto: GetCardsFilterDto): Promise<CardPreviewDto[]> {
-        return this.cardRepository.getCards(filterDto);
+  constructor(@InjectRepository(Card) private cardRepository: CardRepository) {}
+  async getCards(filterDto: GetCardsFilterDto): Promise<CardPreviewDto[]> {
+    return this.cardRepository.getCards(filterDto);
+  }
+
+  async getCardsById(id: number): Promise<Card> {
+    const cardFound = await this.cardRepository.findOne(id);
+
+    if (!cardFound) {
+      throw new NotFoundException(
+        `Healthy Dev no encontró nada con el id ${id}, que esto no te impida seguir haciendo vida sana!`,
+      );
+    } else {
+      return cardFound;
     }
+  }
 }
