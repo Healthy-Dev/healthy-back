9ab8e417bdb5d1d72b3779ae68e14bdf2a6db85a
Andres Di Croche <dicrocheandres@gmail.com>
2020-05-28 22:28:06 -0300

    fix search to case-insensitive

--- src/cards/card.repository.ts
+++ src/cards/card.repository.ts
@@ -4,19 +4,22 @@ import { GetCardsFilterDto } from './dto/get-cards.dto';
 import { CardPreviewDto } from './dto/card-preview.dto';
 @EntityRepository(Card)
 export class CardRepository extends Repository<Card> {
-    getCards(filterDto: GetCardsFilterDto): Promise<CardPreviewDto[]> {
-        const { offset, limit, search } = filterDto;
-        const query = this.createQueryBuilder('card');
-        query.select('card.id, card.title, card.photo');
-        if (search) {
-            query.where('card.title LIKE :search OR card.description LIKE :search', {
-                search: `%${search}%`,
-            });
-        }
-        query.orderBy('card.id', 'DESC');
-        query.skip(offset);
-        query.take(limit);
-        const headerCards: Promise<CardPreviewDto[]> = query.getRawMany();
-        return headerCards;
+  getCards(filterDto: GetCardsFilterDto): Promise<CardPreviewDto[]> {
+    const { offset, limit, search } = filterDto;
+    const query = this.createQueryBuilder('card');
+    query.select('card.id, card.title, card.photo');
+    if (search) {
+      query.where(
+        'LOWER(card.title) LIKE :search OR LOWER(card.description) LIKE :search',
+        {
+          search: `%${search.toLowerCase()}%`,
+        },
+      );
     }
+    query.orderBy('card.id', 'DESC');
+    query.skip(offset);
+    query.take(limit);
+    const headerCards: Promise<CardPreviewDto[]> = query.getRawMany();
+    return headerCards;
+  }
 }
