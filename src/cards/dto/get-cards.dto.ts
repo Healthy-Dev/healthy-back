f7aebfad3f86bd660ebe2cc7dee9e17df73383db
Andres Di Croche <dicrocheandres@gmail.com>
2020-05-30 23:33:40 -0300

    modify error message to spanish

--- src/cards/dto/get-cards.dto.ts
+++ src/cards/dto/get-cards.dto.ts
@@ -1,17 +1,17 @@
 import { IsOptional, IsNotEmpty, IsNumber } from 'class-validator';
 import { Type } from 'class-transformer';
 export class GetCardsFilterDto {
-    @IsOptional()
-    @IsNumber()
-    @Type(() => Number)
-    limit: number = 15;
+  @IsOptional()
+  @IsNumber()
+  @Type(() => Number)
+  limit: number = 15;
 
-    @IsOptional()
-    @IsNumber()
-    @Type(() => Number)
-    offset: number = 0;
+  @IsOptional()
+  @IsNumber()
+  @Type(() => Number)
+  offset: number = 0;
 
-    @IsOptional()
-    @IsNotEmpty()
-    search: string;
+  @IsOptional()
+  @IsNotEmpty({ message: 'La cadena a buscar no puede estar vacía.' })
+  search: string;
 }
