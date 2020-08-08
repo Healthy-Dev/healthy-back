cdb5a673f35e48343852a773bb7acfe790fbeae7
franmper <franp@mantik.tech>
2020-08-08 15:15:26 -0300

    delete boilerplate data

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -6,28 +6,7 @@ export type User = any;
 export class UsersService {
     private readonly users: User[];
 
-    constructor() {
-
-      // test users, until db is connected
-
-        this.users = [
-            {
-                userId: 1,
-                username: 'john',
-                password: 'changeme',
-              },
-              {
-                userId: 2,
-                username: 'chris',
-                password: 'secret',
-              },
-              {
-                userId: 3,
-                username: 'maria',
-                password: 'guess',
-              },
-        ];
-    }
+    constructor() {}
     async findOne(username: string): Promise<User | undefined> {
         return this.users.find(user => user.username === username);
     }
